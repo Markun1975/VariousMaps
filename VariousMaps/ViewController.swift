38ea5e48e0c9001341748b80d1b017886c98425a
= <=>
2022-01-07 22:26:32 +0900

    Initial Commit

---
+++ VariousMaps/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VariousMaps
+//
+//  Created by Masaki on R 4/01/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
