83e77e18a254f4f5ef71011de9a3b66c4341bab9
Markun1975 <choco_9031@yahoo.co.jp>
2022-01-09 23:34:22 +0900

    add UserModel

---
+++ VariousMaps/Models/User.swift
@@ -0,0 +1,15 @@
+//
+//  User.swift
+//  VariousMaps
+//
+//  Created by Masaki on R 4/01/09.
+//
+
+import Foundation
+
+class User {    
+    let id = String()
+    let name = String()
+    let mailAdress = String()
+    let createdAt = String()
+}
