e54ff133fc07a97c084557832b1dc021f19a754c
Markun1975 <choco_9031@yahoo.co.jp>
2022-01-09 23:54:36 +0900

    add WalkList

---
+++ VariousMaps/Models/WalkList.swift
@@ -0,0 +1,14 @@
+//
+//  WalkList.swift
+//  VariousMaps
+//
+//  Created by Masaki on R 4/01/09.
+//
+
+import Foundation
+
+class WalkList {
+    let id = String()
+    let spots = [Spot()]
+    
+}
